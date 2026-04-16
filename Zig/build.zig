94a87193c153fdb08b28481f7386c170d16b0ef1
Florent Michel <florent.m42@gmail.com>
2026-04-16 16:59:22 +0100

    update the target

--- Zig/build.zig
+++ Zig/build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.graph.host;
     const optimize = b.standardOptimizeOption(.{
         .preferred_optimize_mode = .ReleaseFast,
     });
